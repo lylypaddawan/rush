f76d70cc76ae6d349607b6dce2c51aec04566f59
lysandre corjon <corjon_l@pc-corjon_l.(none)>
2013-03-01 21:31:09 +0100

    rev

--- check_file.c
+++ check_file.c
@@ -5,5 +5,33 @@
 ** Login   <corjon_l@epitech.net>
 ** 
 ** Started on  Fri Mar  1 20:54:16 2013 lysandre corjon
-** Last update Fri Mar  1 21:02:47 2013 lysandre corjon
+** Last update Fri Mar  1 21:30:30 2013 lysandre corjon
 */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fctnl.h>
+#include <unistd.h>
+
+int	is_file(char *str)
+{
+  if (acces(str, F_OK, R_OK) != 0)
+    return (-1);
+  return (0);
+}
+
+char	**check_file(char *str)
+{
+  int	fd;
+
+  if (is_dir(str) == 0)
+    {
+      my_perror("MAP ERROR\n");
+      return (NULL);
+    }
+  if ((fd = open(str, O_RDONLY)) == -1)
+    {
+      my_perror("MAP ERROR\n");
+      return (NULL);
+    }
+}
